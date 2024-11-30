7f9cd9ce2a5d41e4aba3abb15c0ec106599df8c2
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-11-30 23:58:38 +0500

    pushing val thr input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React, { useState } from 'react'
-import {useDispatch , useSelector} from 'react-redux'
-import { addTodo } from './configs/redux/reducers/todoSlice'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "./configs/redux/reducers/todoSlice";
 
 const App = () => {
-  const [text, setText] = useState('1st todo')
-  const dispatch = useDispatch()
+  const [text, setText] = useState("1st todo");
+  const dispatch = useDispatch();
   const todos = useSelector((state) => state);
 
-  
-  const handleAddTodo = () => {
-    dispatch(addTodo(text))
-    console.log(todos)
-  }
+  const handleAddTodo = (e) => {
+    if (text.trim()) {
+      e.preventDefault()
+      dispatch(addTodo(text));
+      setText('')
+      console.log(todos);
+    }
+  };
 
   return (
-    <button onClick={handleAddTodo}>Add Todo</button>
-  )
-}
+    <form onSubmit={addTodo}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        required
+      />
 
-export default App
+      <button onClick={handleAddTodo}>Add Todo</button>
+    </form>
+  );
+};
+
+export default App;
